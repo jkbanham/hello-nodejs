19c7ed78d58c74250b43a0b931f72fbd48198bd3
jkbanham <54941260+jkbanham@users.noreply.github.com>
2022-12-10 07:29:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 console.log("Hello World!\n");
 
+console.log("For my project I originally planned on making a application that would ask the user to enter multiple nouns and adjectives " +
+"to a list. The program would then randomly pick one of the adjectives or nouns and insert it into the story. " +
+"When tested the project in visual studios and it ran peferfectly fine.");
+
+console.log("After running it in visual studios I decided it was time to transfer my github ripositroy into openshift. " +
+    "When I ran the bulid I got an error that said error: provided context directory does not exist: /index. " +
+    "I found that the error occured because one of the pods from the cluster was missing a directory in it. " +
+    "I believe that with further research into openshift, I might have been able to run the application.");
